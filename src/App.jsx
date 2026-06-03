0235198b4cb371ec05e88931879ee1d5fd3a6f13
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-02 22:01:12 -0300

    Criando o container principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import logo from "./assets/logo.svg";
+import { Container } from "./components/Container";
 
 function App() {
   return (
-    <>
+    <Container>
       <aside>
         <img src={logo} alt="Logo" />
       </aside>
       <h1>Poupapp</h1>
-    </>
+    </Container>
   );
 }
 
