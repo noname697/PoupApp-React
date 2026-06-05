c4798265a7e6be14c327687e0d3b9190c1134dac
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-05 17:13:17 -0300

    Criando, estruturando e estilizando Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Aside } from "./components/Aside";
+import Card from "./components/Card";
 import { Container } from "./components/Container";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
@@ -13,8 +14,26 @@ const App = () => {
           <SearchInput name="q" placeholder="Procure seu dinheiro..." />
           <div>
             <Typography variant="h1">Olá, Arthur!</Typography>
-            <Typography>Veja como estão suas finanças hoje</Typography>
+            <Typography>Veja como estão suas finanças hoje.</Typography>
           </div>
+          <section>
+            <Card>
+              <Card.Header>Orçamento diário disponível</Card.Header>
+              <Card.Body>R$200</Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Progresso da meta financeira</Card.Header>
+              <Card.Body>R$200</Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Movimentação financeira</Card.Header>
+              <Card.Body>R$200</Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>Minhas contas</Card.Header>
+              <Card.Body>R$200</Card.Body>
+            </Card>
+          </section>
         </Main>
       </Container>
     </div>
