6a22b600f778c8ff86a581770445b0dbe9fde8a7
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-05 17:21:01 -0300

    Criando primeiro Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Aside } from "./components/Aside";
 import Card from "./components/Card";
 import { Container } from "./components/Container";
+import { DailyBudget } from "./components/DailyBudget";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
 import { Typography } from "./components/Typography";
@@ -19,7 +20,9 @@ const App = () => {
           <section>
             <Card>
               <Card.Header>Orçamento diário disponível</Card.Header>
-              <Card.Body>R$200</Card.Body>
+              <Card.Body>
+                <DailyBudget value={42} />
+              </Card.Body>
             </Card>
             <Card>
               <Card.Header>Progresso da meta financeira</Card.Header>
