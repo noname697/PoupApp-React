536d8cfe22ba0b7d0ac8885792645b2b405c9dd4
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-05 10:03:45 -0300

    Adicionando váriaveis de cores globais e adicionando logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import logo from "./assets/logo.svg";
+
 const App = () => {
-
   return (
-    <>
-    
-    </>
-  )
-}
+    <div className="bg-neutral-background min-h-screen">
+      <aside>
+        <img src={logo} alt="Logo" />
+      </aside>
+    </div>
+  );
+};
 
-export default App
+export default App;
